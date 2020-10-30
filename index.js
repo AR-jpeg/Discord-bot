b763f0951d739ecb2e9168227e82c1000a6a171d
Aditya Rao Udupi <araoudupi@gmail.com>
2020-10-30 14:18:07 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+// require the discord.js module
+const Discord = require('discord.js');
+ const { prefix, token } = require('./config.json');
+
+// create a new Discord client
+const client = new Discord.Client();
+client.commands = new Discord.Collection()
+
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'ping') {
+		message.channel.send('Pong.');
+	} else if (command === 'beep') {
+		message.channel.send('Boop.');
+	} else if (command === 'server') {
+		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+	} else if (command === 'user-info') {
+		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+	} else if (command === 'info') {
+		if (!args.length) {
+			return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+		} else if (args[0] === 'foo') {
+			return message.channel.send('bar');
+		}
+
+		message.channel.send(`First argument: ${args[0]}`);
+	} else if (command === 'kick') {
+		if (!message.mentions.users.size) {
+			return message.reply('you need to tag a user in order to kick them!');
+		}
+
+		const taggedUser = message.mentions.users.first();
+
+		message.channel.send(`You wanted to kick: ${taggedUser.username}`);
+	} else if (command === 'avatar') {
+		if (!message.mentions.users.size) {
+			return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ dynamic: true })}>`);
+		}
+
+		const avatarList = message.mentions.users.map(user => {
+			return `${user.username}'s avatar: <${user.displayAvatarURL({ dynamic: true })}>`;
+		});
+
+		message.channel.send(avatarList);
+	} else if (command === 'delete') {
+		const amount = parseInt(args[0]) + 1;
+
+		if (isNaN(amount)) {
+			return message.reply(`That doesn't seem to be a valid number!`);
+		} else if (amount <= 1 || amount > 100) {
+			return message.reply('You need to input a number between 1 and 99.');
+		}
+
+		message.channel.bulkDelete(amount, true).catch(err => {
+			console.error(err);
+			message.channel.send('Oh No! There was an error trying to delete messages in this channel!');
+		});
+	}
+});
+
+client.login(token);
